5b1e55cf728840e59e44ce155ecee125d33d6538
Michal Kaminski <MichalKaminski.KamienKaze@gmail.com>
2023-07-27 22:29:55 +0200

    info module: added dropdown component

--- src/app/modules/info/info.module.ts
+++ src/app/modules/info/info.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InfoSectionComponent } from './info-section/info-section.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
 
 @NgModule({
-  declarations: [InfoSectionComponent],
+  declarations: [InfoSectionComponent, DropdownComponent],
   exports: [InfoSectionComponent],
   imports: [CommonModule],
 })
